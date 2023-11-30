2ea240b7d4b398d633ff0e3d22a4c987a5f03285
toiladao <bthd1805@gmail.com>
2023-11-30 18:11:37 +0700

    init project

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "i3t8gb",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
