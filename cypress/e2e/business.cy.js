ded53003214cf1747ef9d69280db2dc21d421373
toiladao <bthd1805@gmail.com>
2023-12-02 16:43:31 +0700

    add search feature

--- cypress/e2e/business.cy.js
+++ cypress/e2e/business.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('homepage', () => {
+  it('search successful ', () => {
     cy.visit('https://example.cypress.io')
+  })
+  it('search theo tu khoa ', () => {
+    cy.visit('https://example.cypress.io')
+    
+  })
+  it('filter', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains("Kitchen Sink")
   })
 })
