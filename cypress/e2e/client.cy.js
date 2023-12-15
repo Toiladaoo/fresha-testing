e4ed2adb30c22fa2505043cfd1576e216dfb8b2a
GreeN7LeaF <thaomy310702@gmail.com>
2023-12-15 07:33:54 +0700

    commit

---
+++ cypress/e2e/client.cy.js
@@ -0,0 +1,5 @@
+describe("add client", () => {
+  it("valid information ", () => {
+    cy.login("thaomy310702@gmail.com", "hellomyne31");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,8 +10,23 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("https://www.fresha.com");
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get('[data-qa="link-for-business"]').click();
+  cy.get(".hero__topBtnWrapper > .button").click();
+  cy.get('[data-qa="for-businesses"] > .CLn2yq').click();
+  /* ==== End Cypress Studio ==== */
+  cy.wait(500);
+  /* --- enter email account */
+  cy.get(".c96c73cca").type(email);
+  cy.get('[data-qa="continue"]').click();
+  cy.wait(500);
+  // enter password
+  cy.get(".c96c73cca").type(password);
+  cy.get('[data-qa="login"]').click();
+});
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
