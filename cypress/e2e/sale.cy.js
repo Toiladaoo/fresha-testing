19b3850b4ec17034371a2da44462954e1e2b10f1
GreeN7LeaF <thaomy310702@gmail.com>
2023-12-15 10:20:20 +0700

    commit

--- cypress/e2e/sale.cy.js
+++ cypress/e2e/sale.cy.js
@@ -22,7 +22,26 @@ describe("new sales", () => {
       .should("have.length", 1);
   });
 
-  it.only("choose 1 product and add quantity", () => {
+  it("choose 2 product", () => {
+    cy.readFile(Cypress.env("login_json_file")).then((data) => {
+      cy.login_business(data.email, data.password);
+    });
+
+    //go to sale tab
+    cy.get('[data-qa="nav-d-sales"]').click();
+    cy.get('[data-qa="tab-products"]').click();
+    //choose product
+    cy.get('[data-qa="cell-product-name-7269439"]').click();
+
+    //invoice list
+    cy.get("._3aa3d0699 > .gap-default-0")
+      .find('[data-qa*="pos-cart-item-container"]')
+      .should("have.length", 2);
+  });
+
+  it("choose 1 product and add quantity", () => {
+    const expectedText = "2";
+
     cy.readFile(Cypress.env("login_json_file")).then((data) => {
       cy.login_business(data.email, data.password);
     });
@@ -38,5 +57,93 @@ describe("new sales", () => {
       .find('[data-qa*="pos-cart-item-container"]')
       .eq(0)
       .click();
+
+    //add quantity
+    cy.get('[data-qa="cart-edit-quantity-plus"]').click();
+    //check quantity
+    cy.get('[data-qa="cart-edit-quantity-input"]')
+      .should("be.visible")
+      .invoke("val")
+      .should("eq", expectedText);
+  });
+
+  it("choose 1 product and add quantity and save", () => {
+    const expectedText = "2";
+
+    cy.readFile(Cypress.env("login_json_file")).then((data) => {
+      cy.login_business(data.email, data.password);
+    });
+
+    //go to sale tab
+    cy.get('[data-qa="nav-d-sales"]').click();
+    cy.get('[data-qa="tab-products"]').click();
+    //choose product
+    cy.get('[data-qa="cell-product-name-7269439"]').click();
+
+    //invoice list
+    cy.get("._3aa3d0699 > .gap-default-0")
+      .find('[data-qa*="pos-cart-item-container"]')
+      .eq(0)
+      .click();
+
+    //add quantity
+    cy.get('[data-qa="cart-edit-quantity-plus"]').click();
+
+    //save
+    cy.get('[data-qa="pos-cart-edit-save"]').click();
+
+    //check quantity
+    cy.get("._3aa3d0699 > .gap-default-0")
+      .find('[data-qa*="pos-cart-item-container"]')
+      .eq(0)
+      .get('[data-qa="pos-cart-item-amount"]')
+      .should("include.text", "2");
+  });
+
+  it.only("choose 1 product and buy", () => {
+    const tip = "10";
+
+    cy.readFile(Cypress.env("login_json_file")).then((data) => {
+      cy.login_business(data.email, data.password);
+    });
+
+    //go to sale tab
+    cy.get('[data-qa="nav-d-sales"]').click();
+    cy.get('[data-qa="tab-products"]').click();
+    //choose product
+    cy.get('[data-qa="cell-product-name-7269439"]').click();
+
+    //invoice list
+    cy.get("._3aa3d0699 > .gap-default-0")
+      .find('[data-qa*="pos-cart-item-container"]')
+      .eq(0)
+      .click();
+
+    //add quantity
+    cy.get('[data-qa="cart-edit-quantity-plus"]').click();
+
+    //save
+    cy.get('[data-qa="pos-cart-edit-save"]').click();
+    cy.get('[data-qa="cart-continue-button"]').click();
+    //tip
+    let tip_money = 0;
+    if (tip == "0") {
+      cy.get('[data-qa="no-tip-button"] > ._06c620699').click();
+    } else {
+      cy.get(`[data-qa="tip-tile-${tip}"] > ._06c620699`).click();
+      cy.get(
+        '[data-qa="tip-tile-10"] > ._06c620699 > .n5MyOK > .font-default-body-s-regular'
+      )
+        .invoke("text")
+        .then((text) => {
+          cy.log(text.slice(1).replace(/,/g, ""));
+          const money_str = text.slice(1).replace(/,/g, "");
+          tip_money = parseInt(money_str, 10);
+        });
+    }
+
+    //check total money
+    let total = 0;
+    cy.get('[data-qa="pos-cart-to-pay"]');
   });
 });
